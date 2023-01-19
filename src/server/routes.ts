b65286f8febf2e73189d5dc512f965217a92814f
mivasconcelos <mvasconcelos@memsql.com>
2023-01-19 13:33:08 +0000

    test setup route

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -3,7 +3,12 @@ import * as express from 'express';
 const router = express.Router();
 
 router.get('/api/hello', (req, res, next) => {
-    res.json('World');
+    res.json('SingleStore');
+});
+
+router.get('/setup', (req, res, next) => {
+    console.log(req)
+    res.json('SETUP!');
 });
 
 export default router;
