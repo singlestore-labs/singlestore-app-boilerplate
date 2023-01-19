c5a287b8fe8e9a9a81bb8a2053e4dd9d37fd3280
mivasconcelos <mvasconcelos@memsql.com>
2023-01-19 14:34:17 +0000

    wip

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,14 +1,21 @@
 import * as express from 'express';
+const bodyParser = require('body-parser');
 
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+
+app.use(express.json())
 const router = express.Router();
 
 router.get('/api/hello', (req, res, next) => {
     res.json('SingleStore');
 });
 
-router.get('/setup', (req, res, next) => {
-    console.log(req)
-    res.json('SETUP!');
+router.post('/setup', (req, res, next) => {
+    console.log("////", req.body)
+    res.json('/SETUP!');
 });
 
 export default router;
