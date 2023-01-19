b952c52aac2dcc041c33e08df75bf9a94b7da9a1
lcustodio1 <lcustodio@singlestore.com>
2023-01-19 17:44:49 +0000

    simple connection creates database

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,17 +1,23 @@
-import * as express from 'express';
-const bodyParser = require('body-parser');
+import * as express from "express";
+import { connectSingleStore } from "./connection";
+const bodyParser = require("body-parser");
 
 const app = express();
-
 const router = express.Router();
 
-router.get('/api/hello', (req, res, next) => {
-    res.json('SingleStore');
+router.get("/api/hello", (req, res, next) => {
+    res.json("SingleStore");
 });
 
-router.post('/setup', bodyParser.json(),(req, res, next) => {
+router.post("/setup", bodyParser.json(), (req, res, next) => {
     console.log("//setup on the boilerplate side//", { req: req.body });
-        res.json('/SETUP!');
+
+    process.env.HOST = req.body.hostname;
+    process.env.PASSWORD = req.body.password;
+
+    connectSingleStore("todo");
+
+    res.json("/SETUP!");
 });
 
 export default router;
